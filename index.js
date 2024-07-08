39fea97dbeda3af22ae64b9e886dd91ee3f1e7d0
Kitanga Nday <kitanganday@gmail.com>
2024-07-08 04:18:26 +0200

    feat: added new avif changes

---
+++ index.js
@@ -0,0 +1,22 @@
+const { readdir, readFile } = require("node:fs/promises");
+const { join } = require('node:path');
+const sharp = require('sharp');
+
+const img_folder = join(import.meta.dir, 'img');
+const preprocessed_img_folder = join(import.meta.dir, 'preprocessed');
+
+// read all the files in the current directory
+const preprocessed_files = await readdir(preprocessed_img_folder);
+
+console.log(preprocessed_files);
+
+preprocessed_files.forEach(async file => {
+    readFile(join('preprocessed', file)).then(buf => {
+        const sharpFile = sharp(buf);
+
+        sharpFile
+            .avif({
+                effort: 7
+            }).toFile(join('img', file.replace(/.webp|.png/g, '.avif')));
+    });
+})
